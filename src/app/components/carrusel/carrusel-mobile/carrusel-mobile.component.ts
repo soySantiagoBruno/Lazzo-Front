f929e94232b3d7a51cdc126725b075d32be04a35
Santiago Bruno <soysantiagobruno@gmail.com>
2025-12-20 19:30:08 -0300

    comienzo nuevo componente de carrusel-mobile

---
+++ src/app/components/carrusel/carrusel-mobile/carrusel-mobile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrusel-mobile.component.html',
+  styleUrl: './carrusel-mobile.component.css'
+})
+export class CarruselMobileComponent {
+
+}
