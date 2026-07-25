db76a4ee482a6934a2c377c4624e0210ab41f396
Santiago Bruno <soysantiagobruno@gmail.com>
2026-07-25 20:29:56 -0300

    modifico navbar para mostrar ciertos botones dependiendo de si esta logueado o no

--- src/app/components/navbars/navbar/navbar.component.ts
+++ src/app/components/navbars/navbar/navbar.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  isAuthenticated: boolean = false;
+
+  constructor(
+    private userService: UserService, 
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+
+    // controla si se van a mostrar en el DOM los li "login" y "ver mis mascotas publicadas" dependiendo si el usuario está logueado o no.
+    if (await this.userService.getUsuario()){
+      this.isAuthenticated = true;
+    }
+  }
+  
 
 }
