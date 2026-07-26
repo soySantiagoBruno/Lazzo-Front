650c800742673721127cb4c4b495b0ef7a4caeff
Santiago Bruno <soysantiagobruno@gmail.com>
2026-07-26 00:03:17 -0300

    ahora se muestran navbars distintos dependiendo de si esta logueado no

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from "../footers/footer/footer.component";
 import { CarruselComponent } from "../carrusel/carrusel.component";
-import { InfoMascotaComponent } from "../info-mascota/info-mascota.component";
 import { NavbarComponent } from "../navbars/navbar/navbar.component";
-import { RouterLink } from '@angular/router';
 import { FiltroComponent } from '../filtro/filtro.component';
 import { AboutComponent } from "../about/about.component";
+import { NavbarUsuarioComponent } from "../navbars/navbar-usuario/navbar-usuario.component";
+import { UsuarioRegisterDto } from '../../models/usuario-register-dto';
+import { UserService } from '../../services/user.service';
+import { onAuthStateChanged } from 'firebase/auth';
+import { Auth } from '@angular/fire/auth';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [FooterComponent, CarruselComponent, InfoMascotaComponent, FiltroComponent, NavbarComponent, RouterLink, AboutComponent, AboutComponent]
+    imports: [FooterComponent, CarruselComponent, FiltroComponent, NavbarComponent, AboutComponent, NavbarUsuarioComponent]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    estaAutenticado = false;
+    
+    constructor(
+        private userService: UserService,
+        private auth: Auth
+    ){}
+
+    async ngOnInit(): Promise<void> {
+        onAuthStateChanged(this.auth, (user) => {
+            if (user) {
+                this.estaAutenticado = true;
+                console.log("El usuario ha iniciado sesión:", this.estaAutenticado);
+            } else {
+                this.estaAutenticado = false;
+                console.log("No hay usuario activo:", this.estaAutenticado);
+            }
+        });
+    }
 
 }
